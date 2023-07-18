7420de1be3d60acce159fd5f067f9af64a7a4a87
zhongliang00 <zhongliang@pm.me>
2023-07-18 17:47:20 +0800

    added skeleton for getRecs

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,31 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
+function getRecs(location: string, days: number) {
+  return [["merlion", "sentosa"], ["orchard road", "hawker center"]]
 }
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log(req.query)
+  const query = req.query
+  const location = query?.location
+  const daysString = query?.days
+  if (!location || !daysString || Array.isArray(location) || Array.isArray(daysString)) {
+    res.status(500).json({ "error_code": "bad request" })
+    return
+  }
+
+  const days = parseInt(daysString, 10);
+  if (Number.isNaN(days) || days < 1 || days > 5) {
+    res.status(500).json({ "error_code": "bad request" })
+    return
+  }
+
+  const results = getRecs(location, days)
+  res.status(200).json(results)
+
+
 }
