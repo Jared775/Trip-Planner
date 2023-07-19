4752c00221bf331c2ef606e3587e89aa7b188ce1
Jared <jaredryanlee@gmail.com>
2023-07-18 20:18:40 -0700

    added openai dependency added endpoint for getting recommendations

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,15 +1,51 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import {Configuration, OpenAIApi} from "openai";
 
-function getRecs(location: string, days: number) {
-  return [["merlion", "sentosa"], ["orchard road", "hawker center"]]
+async function getRecs(location: string, days: number) {
+
+  const openai = new OpenAIApi(
+      new Configuration({
+        apiKey: "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"//process.env.OPENAI_API_KEY,
+      })
+  );
+
+  const rawResponse = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo-0613",
+    messages: [
+      {
+        role: "user",
+        content:
+            "plan a 5 day itinerary for tokyo japan, but do not print it, extract only the key words that are places in the itinerary and list them out with bullets. also remove the days",
+      },
+    ],
+    max_tokens: 3500,
+    temperature: 0,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+  const response = rawResponse.data
+
+
+  const content: string = response.choices[0].message.content;
+  const contentArray = content.split("\n");
+  for (let i = 0; i < contentArray.length; i++) {
+    if (contentArray[i][0] === '-') {
+      contentArray[i] = contentArray[i].slice(2)
+    }
+  }
+  //console.log(contentArray);
+
+
+  return contentArray
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log(req.query)
+  //console.log(req.query)
   const query = req.query
   const location = query?.location
   const daysString = query?.days
@@ -24,7 +60,7 @@ export default function handler(
     return
   }
 
-  const results = getRecs(location, days)
+  const results = await getRecs(location, days)
   res.status(200).json(results)
 
 
