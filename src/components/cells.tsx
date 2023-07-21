5f0b3594d53857f7cc8f50be15ce4e50b7af6281
zhongliang00 <zhongliang@pm.me>
2023-07-21 16:10:41 +0800

    add drag and drop display component

---
+++ src/components/cells.tsx
@@ -0,0 +1,140 @@
+import React, { Dispatch, SetStateAction } from "react";
+import { DragDropContext, Draggable, DraggableLocation, DraggingStyle, Droppable, DropResult, NotDraggingStyle } from "react-beautiful-dnd";
+import { Property } from "csstype";
+import UserSelect = Property.UserSelect;
+
+export type CellItem = { id: string; content: string };
+
+const reorder = (list: CellItem[], startIndex: number, endIndex: number) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+/**
+ * Moves an item from one list to another list.
+ */
+const move = (source: CellItem[], destination: CellItem[], droppableSource: DraggableLocation, droppableDestination: DraggableLocation) => {
+  const sourceClone = Array.from(source);
+  const destClone = Array.from(destination);
+  const [removed] = sourceClone.splice(droppableSource.index, 1);
+
+  destClone.splice(droppableDestination.index, 0, removed);
+
+  const result: Record<string, CellItem[]> = {};
+  result[droppableSource.droppableId] = sourceClone;
+  result[droppableDestination.droppableId] = destClone;
+
+  return result;
+};
+
+const getItemStyle = (isDragging: boolean, draggableStyle: DraggingStyle | NotDraggingStyle | undefined) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none" as UserSelect,
+  // padding: grid * 2,
+  // margin: `0 0 ${grid}px 0`,
+  //
+  // // change background colour if dragging
+  // background: isDragging ? "lightgreen" : "grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle
+});
+const getListStyle = (isDraggingOver: boolean | undefined) => ({
+  // background: isDraggingOver ? "lightblue" : "lightgrey",
+  // padding: grid,
+  // width: 250
+});
+
+export function Cells(props: { cellsData: CellItem[][], setCellsData: Dispatch<SetStateAction<CellItem[][]>> }) {
+
+  const { cellsData, setCellsData } = props;
+
+  function onDragEnd(result: DropResult) {
+    const { source, destination } = result;
+
+    // dropped outside the list
+    if (!destination) {
+      return;
+    }
+    const sInd = +source.droppableId;
+    const dInd = +destination.droppableId;
+
+    if (sInd === dInd) {
+      const items = reorder(cellsData[sInd], source.index, destination.index);
+      const newState = [...cellsData];
+      newState[sInd] = items;
+      setCellsData(newState);
+    } else {
+      const result = move(cellsData[sInd], cellsData[dInd], source, destination);
+      const newState = [...cellsData];
+      newState[sInd] = result[sInd];
+      newState[dInd] = result[dInd];
+
+      setCellsData(newState.filter(group => group.length));
+    }
+  }
+  return (
+    <div className={`grid grid-rows-${cellsData.length}`}>
+      <DragDropContext onDragEnd={onDragEnd}>
+        {cellsData.map((el, ind) => (
+          <Droppable key={ind} droppableId={`${ind}`}>
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                className={"block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"}
+                style={getListStyle(snapshot.isDraggingOver)}
+                {...provided.droppableProps}
+              >
+                {el.map((item, index) => (
+                  <Draggable
+                    key={item.id}
+                    draggableId={item.id}
+                    index={index}
+                  >
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style
+                        )}
+                      >
+                        <div
+                          style={{
+                            display: "flex",
+                            justifyContent: "space-around"
+                          }}
+                        >
+                          {item.content}
+                          <button
+                            type="button"
+                            onClick={() => {
+                              const newState = [...cellsData];
+                              newState[ind].splice(index, 1);
+                              setCellsData(
+                                newState.filter(group => group.length)
+                              );
+                            }}
+                          >
+                            delete
+                          </button>
+                        </div>
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        ))}
+      </DragDropContext>
+    </div>
+  );
+}
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { FieldValues, useForm } from "react-hook-form";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { CellItem, Cells } from "@/components/cells";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
-  const [content, setContent] = useState<string[][]>([])
+  const [content, setContent] = useState<CellItem[][]>([])
 
   const {             //calls on the object useForm() and assigns some of its attributes to the variables below
     register,         //calls on the register attribute of the object, useForm()
@@ -26,13 +26,24 @@ export default function Home() {
       });
 
       // Access the response data
-      setContent(response.data)
+      const itinerary: string[][] = response.data
+      setContent(
+        itinerary.map((day) => (
+          day.map((attraction) => (
+            { id: attraction, content: attraction }
+          ))
+        ))
+      )
       // Process the data further as needed
 
     } catch (error) {
       console.error('Error:', error);
     }
   }
+  const [windowLoaded, setWindowLoaded] = useState(false);
+  useEffect(() => {
+    setWindowLoaded(true)
+  }, [])
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
@@ -57,11 +68,11 @@ export default function Home() {
             <svg aria-hidden="true" role="status" className="inline w-4 h-4 mr-3 text-white animate-spin"
                  viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
-        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-        fill="#E5E7EB"/>
+      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+      fill="#E5E7EB" />
     <path
-        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-        fill="currentColor"/>
+      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+      fill="currentColor" />
     </svg>
 
           </span>}
@@ -69,9 +80,12 @@ export default function Home() {
         </button>
       </form>
 
-      <div className="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">{JSON.stringify(content)}</p>
-      </div>
+      {/*<div className="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">*/}
+      {/*  <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">{JSON.stringify(content)}</p>*/}
+      {/*</div>*/}
+      {windowLoaded &&
+        <Cells setCellsData={setContent} cellsData={content} />
+      }
     </main>
   )
 }
